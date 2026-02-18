aeaf711f48581a787c2e028a834017191dc871c2
MystIsle <pelhav@gmail.com>
2026-02-18 20:52:58 +0900

    [MISC] 코드정리

--- CH2_TeamProject.cpp
+++ CH2_TeamProject.cpp
@@ -11,18 +11,24 @@ using namespace std;
 int main()
 {
 	ABattleManager* BattleManager = new ABattleManager();
-	ACharacter* Player = new APlayer("용사", FUnitStat(200, 50, 30, 5, 10));
-	ACharacter* Monster = new AMonster("몬스터", FUnitStat(100, 30, 20, 3, 10));
+
+	FUnitStat PlayerStat(200, 50, 30, 5, 10);
+	FUnitStat MonsterStat(100, 30, 20, 3, 10);
+
+	ACharacter* Player = new APlayer("용사", PlayerStat);
+	ACharacter* Monster = new AMonster("몬스터", MonsterStat);
 
 	cout << "===  데스매치 시작!  ===" << endl;
-	
+
 	BattleManager->WaitForPlayerInput();
 	BattleManager->RunBattle(Player, Monster);
-	BattleManager-> WaitForPlayerInput();
-	
+	BattleManager->WaitForPlayerInput();
+
+	cout << "===  데스매치 종료!  ===" << endl;
+
 	delete Player;
 	delete Monster;
 	delete BattleManager;
-	
+
 	return 0;
 }
