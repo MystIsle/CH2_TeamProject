f1d1d663b77509583a7d9970c05352aa20911af7
MystIsle <pelhav@gmail.com>
2026-02-18 22:57:41 +0900

    [실습] 스마트 포인터를 적용해보자.

--- CH2_TeamProject.cpp
+++ CH2_TeamProject.cpp
@@ -1,5 +1,5 @@
-﻿#include <Windows.h>
-#include <iostream>
+﻿#include <iostream>
+#include <memory>
 
 #include "BattleManager.h"
 #include "Character/Character.h"
@@ -10,25 +10,21 @@ using namespace std;
 
 int main()
 {
-	ABattleManager* BattleManager = new ABattleManager();
+	auto BattleManager = make_unique<ABattleManager>();
 
 	FUnitStat PlayerStat(200, 50, 30, 5, 10);
 	FUnitStat MonsterStat(100, 30, 20, 3, 10);
 
-	ACharacter* Player = new APlayer("용사", PlayerStat);
-	ACharacter* Monster = new AMonster("몬스터", MonsterStat);
+	unique_ptr<ACharacter> Player = make_unique<APlayer>("용사", PlayerStat);
+	unique_ptr<ACharacter> Monster = make_unique<AMonster>("몬스터", MonsterStat);
 
 	cout << "===  데스매치 시작!  ===" << endl;
 
 	BattleManager->WaitForPlayerInput();
-	BattleManager->RunBattle(Player, Monster);
+	BattleManager->RunBattle(Player.get(), Monster.get());
 	BattleManager->WaitForPlayerInput();
 
 	cout << "===  데스매치 종료!  ===" << endl;
-
-	delete Player;
-	delete Monster;
-	delete BattleManager;
 
 	return 0;
 }
